b6791e37af412ce0327f2c16def4f0ab896483ab
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-10-04 16:39:24 +0900

    React post css

--- template/src/app.jsx
+++ template/src/app.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./app.css";
+import Button1 from "./components/button1";
+import Button2 from "./components/button2";
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+    </>
+  );
 }
 
 export default App;
