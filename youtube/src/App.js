47ca25fd5edf18e442641ec845ae79e3ddc9c602
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-10-04 23:35:31 +0900

    React youtube project start

---
+++ youtube/src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [video, setVideo] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBg7t4jIo0SPuwAsi8CO8EMCFSFG5KBYMk",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideo(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <h1>Hello</h1>;
+};
+
+export default App;
