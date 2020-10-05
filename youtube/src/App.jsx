ec069f0c45de4fb58ccd3db6d83fdd819fd9ec6b
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-10-05 14:54:13 +0900

    React youtube-project 검색기능 구현하기

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -1,8 +1,27 @@
 import React, { useEffect, useState } from "react";
+import SearchHeader from "./components/search-header/Search_Header";
 import VideoList from "./components/video_list/Video_List";
+import styles from "./App.module.css";
 
 const App = () => {
   const [videos, setVideo] = useState([]);
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&key=AIzaSyBg7t4jIo0SPuwAsi8CO8EMCFSFG5KBYMk`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideo(items))
+      .catch((error) => console.log("error", error));
+  };
 
   useEffect(() => {
     const requestOptions = {
@@ -18,7 +37,12 @@ const App = () => {
       .then((result) => setVideo(result.items))
       .catch((error) => console.log("error", error));
   }, []);
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />;
+    </div>
+  );
 };
 
 export default App;
