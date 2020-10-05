9d42169421a02c7f424156b7a690c0e621dcc3bd
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-10-05 15:29:38 +0900

    React youtube-project 검색기능 리팩토링

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -3,39 +3,19 @@ import SearchHeader from "./components/search-header/Search_Header";
 import VideoList from "./components/video_list/Video_List";
 import styles from "./App.module.css";
 
-const App = () => {
+const App = ({ youtube }) => {
   const [videos, setVideo] = useState([]);
-  const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
 
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&key=AIzaSyBg7t4jIo0SPuwAsi8CO8EMCFSFG5KBYMk`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideo(items))
-      .catch((error) => console.log("error", error));
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => setVideo(videos));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBg7t4jIo0SPuwAsi8CO8EMCFSFG5KBYMk",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideo(result.items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideo(videos));
   }, []);
   return (
     <div className={styles.app}>
