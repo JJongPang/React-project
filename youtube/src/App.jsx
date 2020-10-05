91ce355ffb7504ba94f9047eb32039f343e5139b
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-10-05 17:59:43 +0900

    React youtube-project 비디오 선택 & 상세 화면, 다시 목록으로 돌아가기

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -2,11 +2,18 @@ import React, { useEffect, useState } from "react";
 import SearchHeader from "./components/search-header/Search_Header";
 import VideoList from "./components/video_list/Video_List";
 import styles from "./App.module.css";
+import Video_Detail from "./components/video_detail/Video_Detail";
 
 const App = ({ youtube }) => {
   const [videos, setVideo] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
 
   const search = (query) => {
+    setSelectedVideo(null);
     youtube
       .search(query) //
       .then((videos) => setVideo(videos));
@@ -20,7 +27,21 @@ const App = ({ youtube }) => {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />;
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <Video_Detail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+          ;
+        </div>
+      </section>
     </div>
   );
 };
