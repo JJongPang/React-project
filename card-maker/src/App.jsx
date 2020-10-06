1bc6415b1d3079d2bdd4bccb49b2ea65301656be
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-10-06 21:06:23 +0900

    React card-maker 로그인, 로그아웃, Router로 화면 전환

--- card-maker/src/App.jsx
+++ card-maker/src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Login from "./component/login/login";
 import styles from "./App.module.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Maker from "./component/maker/maker";
 
 const App = ({ authService }) => {
   return (
     <div className={styles.app}>
-      <Login authService={authService} />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login authService={authService} />
+          </Route>
+          <Route exact path="/maker">
+            <Maker authService={authService} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 };
